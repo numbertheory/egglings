f82ea406475a158f97c29271e83edd84addfa49b
John-Peter Etcheber <jetcheber@gmail.com>
2020-12-26 16:25:25 -0800

    exception handling starting to come together

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from datetime import datetime, timedelta
+import yaml
+import subprocess
 
+def load_exercise_order():
+    with open("exercises/exercises.yaml") as f:
+        exercises = yaml.safe_load(f)
+    return exercises
 
+def check_exercises():
+    for exercise in exercises:
+        cmd = "python3 exercises/{exercise}.py".format(exercise=exercise)
+        try:
+            exit_code = subprocess.call(cmd,
+                shell=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT)
+            if exit_code != 0:
+                return subprocess.check_output(cmd, shell=True)
+        except:
+            pass
 
 class ModificationWatcher(FileSystemEventHandler):
     def __init__(self):
@@ -20,12 +38,16 @@ class ModificationWatcher(FileSystemEventHandler):
 
 
 if __name__ == "__main__":
+    exercises = load_exercise_order().get('exercises', [])
     event_handler = ModificationWatcher()
     observer = Observer()
     observer.schedule(event_handler, path='exercises/', recursive=False)
     observer.start()
 
     try:
+        exercise_check = check_exercises()
+        if exercise_check:
+            print(exercise_check)
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
